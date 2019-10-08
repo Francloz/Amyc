3b5459719a94948724445a4a750069ca6c01d5bb
francloz <francloz@ucm.es>
2019-10-08 11:33:10 +0200

    Many tests added. Comment parsing

--- test/scala/amyc/test/LexerTests.scala
+++ test/scala/amyc/test/LexerTests.scala
@@ -11,7 +11,22 @@ class LexerTests extends TestSuite {
   val outputExt = "txt"
 
   @Test def testKeywords = shouldOutput("Keywords")
+  @Test def testMultComments = shouldOutput("MultilinedComment")
+  @Test def testOneComments = shouldOutput("OneLinedComment")
+  @Test def testComments = shouldOutput("Comments")
 
+  @Test def testArithmetic = shouldOutput("Arithmetic")
+  @Test def testBinaryOperations = shouldOutput("BinaryOperations")
+  @Test def testFactorial = shouldOutput("Factorial")
+  @Test def testHello = shouldOutput("Hello")
+  @Test def testHelloInt = shouldOutput("HelloInt")
+  @Test def testInvalidOperations = shouldOutput("InvalidOperations")
+  @Test def testLocality = shouldOutput("Locality")
+  @Test def testPrinting = shouldOutput("Printing")
+  @Test def testTestLists = shouldOutput("TestLists")
+
+  @Test def testNestedComments = shouldFail("NestedComments")
+  @Test def testUnclosedComment = shouldFail("UnclosedComment")
   @Test def testSingleAmp = shouldFail("SingleAmp")
 
 }
