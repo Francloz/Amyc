0e8da626c5aec2f649a8ebdd06911742e4ee85e0
lozano <francisco.lozanodelmoral@epfl.ch>
2019-10-10 09:07:25 +0200

    First Commit Parser

--- src/amyc/parsing/Lexer.scala
+++ src/amyc/parsing/Lexer.scala
@@ -9,6 +9,8 @@ import scallion.input._
 
 import amyc.utils.Position
 
+import scala.math.BigInt
+
 // The lexer for Amy.
 object Lexer extends Pipeline[List[File], Iterator[Token]]
                 with Lexers[Token, Char, SourcePosition] {
@@ -70,7 +72,7 @@ object Lexer extends Pipeline[List[File], Iterator[Token]]
     // +  -  *  / %  <  <= && ||  ==  ++ -  !
     word(">=") | word("<=") | word("==") |
     word("++") | word("&&") | word("||") 
-    | oneOf("*+-/%<>") |> { (cs, range) => OperatorToken(cs.mkString).setPos(range._1) },
+    | oneOf("*+-/%<>!") |> { (cs, range) => OperatorToken(cs.mkString).setPos(range._1) },
     
     // Identifiers
     // alpha alphanum*
@@ -80,16 +82,18 @@ object Lexer extends Pipeline[List[File], Iterator[Token]]
 
     // Integer literals
     many1(oneOf("1234567890")) 
-    |> { (cs, range) => if (cs.mkString.toInt > 2147483647) IntLitToken(cs.mkString.toInt).setPos(range._1) else ErrorToken("Bit overflow").setPos(range._1) },
+    |> { (cs, range) => if (cs.mkString.length < 10 && BigInt(cs.mkString) <= 2147483647) IntLitToken(cs.mkString.toInt).setPos(range._1) else ErrorToken("Int overflow (the maximum integer is 2147483647): " ++ cs.mkString).setPos(range._1) },
 
     // String literals
     // All characters except "
-    elem('\"') ~ many(oneOf("0123456789qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ")) 
-    ~ elem('\"') |> { (cs, range) => StringLitToken(cs.mkString).setPos(range._1) },
+    elem('\"') ~ many(elem(_ != '\"')) ~ elem('\"') |> { (cs, range) => StringLitToken(cs.mkString.slice(1, cs.mkString.length - 1)).setPos(range._1) },
+    // Unclosed string
+    elem('\"') ~ many(elem(_ != '\"')) |> { (cs, range) => ErrorToken(cs.mkString.slice(1, cs.mkString.length - 1)).setPos(range._1) },
     
+
     // Delimiters and whitespace
     // White spaces
-    many1(elem(' ')) |>  { (cs, range) => SpaceToken().setPos(range._1) },
+    many1(oneOf(" \n\r")) |>  { (cs, range) => SpaceToken().setPos(range._1) },
     // Delimiters .,:;(){}[]= and =>
     word("=>") | oneOf(".,:;(){}[]=") |>  { (cs, range) => DelimiterToken(cs.mkString).setPos(range._1) },
 
@@ -98,17 +102,28 @@ object Lexer extends Pipeline[List[File], Iterator[Token]]
       |> { cs => CommentToken(cs.mkString("")) },
 
     // Multiline comments
-    // Find nested comments
-    word("/*") ~ many(oneOf("0123456789qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ")) 
-    ~ word("/*") |>  { (cs, range) => ErrorToken("Nested comment").setPos(range._1) },
-    // Find proper comments
-    word("/*") ~ many(oneOf("0123456789qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ")) 
-    ~ word("*/") |>  { (cs, range) => CommentToken(cs.mkString).setPos(range._1) },
-    // Find unclosed comments
-    word("/*") ~ many(oneOf("0123456789qwertyuiopasdfghjklzxcvbnmABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ ")) 
-    |>  { (cs, range) => ErrorToken("Unclosed comment").setPos(range._1) },
 
-    //       Make sure that unclosed multi-line comments result in an ErrorToken.
+    // Find nested comments
+    // "/*" ~ ( many(anyChar \ {"/", "*"}) 
+    //          | many1("/") ~ anyChar
+    //          | many1("*") ~ anyChar ) ~ many1("/") ~ "*"
+    word("/*") ~ many(elem(x => x != '/' && x != '*')
+    | (many1(word("/"))  ~  elem(x => x != '/' && x != '*')) 
+    | (many1(word("*"))  ~  elem(x => x != '/' && x != '*'))) 
+    ~ (many1(oneOf("/")) ~ word("*")) |>  { (cs, range) => ErrorToken("Error token. Found nested comment: " ++ cs.mkString.slice(0, math.min(cs.mkString.length, 10)) ++ "...").setPos(range._1) },
+    
+    // Find proper comments
+    // "/*" ~ ( many(anyChar \ {"/", "*"}) 
+    //          | many1("/") ~ anyChar
+    //          | many1("*") ~ anyChar ) ~ many1("*") ~ "/"
+    word("/*") ~ many(elem(x => x != '/' && x != '*')
+    | (many1(word("/"))  ~  elem(x => x != '/' && x != '*')) 
+    | (many1(word("*"))  ~  elem(x => x != '/' && x != '*'))) 
+    ~ (many1(oneOf("*")) ~ word("/")) |>  { (cs, range) => CommentToken(cs.mkString).setPos(range._1) },
+    
+    // Find unclosed comments
+    word("/*") |>  { (cs, range) => ErrorToken("Error token. Unclosed comment: " ++ cs.mkString.slice(0,  math.min(cs.mkString.length, 10)) ++ "...").setPos(range._1)},
+    // Make sure that unclosed multi-line comments result in an ErrorToken.
   ) onError {
     // We also emit ErrorTokens for Scallion-handled errors.
     (cs, range) => ErrorToken(cs.mkString).setPos(range._1)
---
+++ src/amyc/parsing/Parser.scala
@@ -0,0 +1,121 @@
+package amyc
+package parsing
+
+import scala.language.implicitConversions
+
+import amyc.ast.NominalTreeModule._
+import amyc.utils._
+import Tokens._
+import TokenKinds._
+
+import scallion.syntactic._
+
+// The parser for Amy
+object Parser extends Pipeline[Iterator[Token], Program]
+                 with Syntaxes[Token, TokenKind] with Debug[Token, TokenKind]
+                 with Operators {
+  import Implicits._
+
+  override def getKind(token: Token): TokenKind = TokenKind.of(token)
+
+  val eof: Syntax[Token] = elem(EOFKind)
+  def op(string: String): Syntax[String] = accept(OperatorKind(string)) { case OperatorToken(name) => name }
+  def kw(string: String): Syntax[Token] = elem(KeywordKind(string))
+
+  implicit def delimiter(string: String): Syntax[Token] = elem(DelimiterKind(string))
+
+  // An entire program (the starting rule for any Amy file).
+  lazy val program: Syntax[Program] = many1(many1(module) ~<~ eof).map(ms => Program(ms.flatten.toList).setPos(ms.head.head))
+
+  // A module (i.e., a collection of definitions and an initializer expression)
+  lazy val module: Syntax[ModuleDef] = (kw("object") ~ identifier ~ "{" ~ many(definition) ~ opt(expr) ~ "}").map {
+    case obj ~ id ~ _ ~ defs ~ body ~ _ => ModuleDef(id, defs.toList, body).setPos(obj)
+  }
+
+  // An identifier.
+  val identifier: Syntax[String] = accept(IdentifierKind) {
+    case IdentifierToken(name) => name
+  }
+
+  // An identifier along with its position.
+  val identifierPos: Syntax[(String, Position)] = accept(IdentifierKind) {
+    case id@IdentifierToken(name) => (name, id.position)
+  }
+
+  // A definition within a module.
+  lazy val definition: Syntax[ClassOrFunDef] = ???
+
+  // A list of parameter definitions.
+  lazy val parameters: Syntax[List[ParamDef]] = repsep(parameter, ",").map(_.toList)
+
+  // A parameter definition, i.e., an identifier along with the expected type.
+  lazy val parameter: Syntax[ParamDef] = ???
+
+  // A type expression.
+  lazy val typeTree: Syntax[TypeTree] = primitiveType | identifierType
+
+  // A built-in type (such as `Int`).
+  val primitiveType: Syntax[TypeTree] = accept(PrimTypeKind) {
+    case tk@PrimTypeToken(name) => TypeTree(name match {
+      case "Unit" => UnitType
+      case "Boolean" => BooleanType
+      case "Int" => IntType
+      case "String" => StringType
+      case _ => throw new java.lang.Error("Unexpected primitive type name: " + name)
+    }).setPos(tk)
+  }
+
+  // A user-defined type (such as `List`).
+  lazy val identifierType: Syntax[TypeTree] = ???
+
+
+  // An expression.
+  // HINT: You can use `operators` to take care of associativity and precedence
+  lazy val expr: Syntax[Expr] = recursive { ??? }
+
+  // A literal expression.
+  lazy val literal: Syntax[Literal[_]] = ???
+
+  // A pattern as part of a mach case.
+  lazy val pattern: Syntax[Pattern] = recursive {
+    literalPattern | wildPattern | ???
+  }
+
+  lazy val literalPattern: Syntax[Pattern] = ???
+
+  lazy val wildPattern: Syntax[Pattern] = ???
+
+
+  // HINT: It is useful to have a restricted set of expressions that don't include any more operators on the outer level.
+  lazy val simpleExpr: Syntax[Expr] = literal.up[Expr] | variableOrCall | ???
+
+  lazy val variableOrCall: Syntax[Expr] = ???
+
+
+  // TODO: Other definitions.
+  //       Feel free to decompose the rules in whatever way convenient.
+
+
+  // Ensures the grammar is in LL(1), otherwise prints some counterexamples
+  lazy val checkLL1: Boolean = {
+    if (program.isLL1) {
+      true
+    } else {
+      debug(program)
+      false
+    }
+  }
+
+  override def run(ctx: Context)(tokens: Iterator[Token]): Program = {
+    import ctx.reporter._
+    if (!checkLL1) {
+      ctx.reporter.fatal("Program grammar is not LL1!")
+    }
+
+    program(tokens) match {
+      case Parsed(result, rest) => result
+      case UnexpectedEnd(rest) => fatal("Unexpected end of input.")
+      case UnexpectedToken(token, rest) => fatal("Unexpected token: " + token + ", possible kinds: " + rest.first.map(_.toString).mkString(", "))
+    }
+  }
+}
