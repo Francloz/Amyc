07fe53bece16e2a5e20a11e3c3012922aebd16db
lozano <francisco.lozanodelmoral@epfl.ch>
2019-12-17 23:48:59 +0100

    Webassambly

--- examples/MyTest.scala
+++ examples/MyTest.scala
@@ -1,16 +1,17 @@
-object ParamAndLocal {
-  def Foo(i: Int): Int = {
-    val i: Int = i + 1;
-    (val i: Int = 2; 
-    32 + i)
-  }
-  
-  abstract class List
-  case class nil() extends List
-  case class Cons(h: Int, t: List) extends List
+object MyTest {
+  // def x(i : Int, j : Int) : Int = {i*j}
+  // def y() : Int = {3}
 
-  Cons(3, nil()) match {
-    case Cons(h1, Cons(h2, t)) => 2
-    case nil() => Foo(3)
-  }
+  abstract class List
+  case class Nil() extends List
+  case class Cons(h: Boolean, t: List) extends List
+  //val ys : List = Cons(3, Nil());
+  //val xs : List = Cons(2, ys);
+  val l : List = Cons(true, Nil()); //Nil();
+  val correct : Boolean = l match {
+    case Cons(x,_) => x
+    case Nil() => false
+  };
+  //val correct : Boolean = true;
+  if(correct){error("Yes")}else{error("No")}
 }
